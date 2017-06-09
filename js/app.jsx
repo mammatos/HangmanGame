6cd2aaf848bce3edc4be3d8f4d67c79ebff9b174
Mammatos <mariannawaranska@gmail.com>
2017-06-09 12:15:21 +0200

    upload files

---
+++ js/app.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Router,
+Route,
+Link,
+IndexLink,
+IndexRoute,
+hashHistory
+} from 'react-router';
+
+// Import components
+
+
+
+class App extends Component {
+
+    render() {
+        return <div  />
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    
+    ReactDOM.render(
+        <App/>,
+        document.getElementById('app')
+    );
+
+});
